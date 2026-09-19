d6848bd81fffbad2ab2071ca51017f70b151fd2e
zhaoyuchong <zyclyzdc@icloud.com>
2026-09-19 23:00:20 +0800

    Add offline English word practice companion

---
+++ english/sw.js
@@ -0,0 +1,5 @@
+'use strict';
+const CACHE='star-english-860cc750be6e',ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('star-english-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;if(e.request.mode==='navigate'){e.respondWith(caches.open(CACHE).then(async c=>(await c.match('./index.html'))||fetch(e.request)));return;}e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request)));});
