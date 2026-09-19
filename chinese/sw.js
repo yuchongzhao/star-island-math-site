a9d7a0c5c88457aad3f858fb2f7c8569069ca986
zhaoyuchong <zyclyzdc@icloud.com>
2026-09-20 06:55:12 +0800

    Add live session time guidance

--- chinese/sw.js
+++ chinese/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='star-chinese-72105bf73755',ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='star-chinese-d5283b55bb5a',ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('star-chinese-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
