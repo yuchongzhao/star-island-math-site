8e6dfe8e6b2544d71f8b5b2f183479c41eaeec46
zhaoyuchong <zyclyzdc@icloud.com>
2026-09-20 23:19:31 +0800

    Allow one-time activation inside a separate iPad home-screen storage context

--- chinese/sw.js
+++ chinese/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='star-chinese-522973f76176',ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='star-chinese-7cf6b87ad2af',ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('star-chinese-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
